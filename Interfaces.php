044d7d282bc966fbc35508c3ed1493912b1b7470
Steffen Sørensen <steffen@sublife.dk>
2006-06-09 14:39:32 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/tags/2_1_0@2237 516bd117-9ffd-0310-910c-af5d773169db

---
+++ Interfaces.php
@@ -0,0 +1,45 @@
+<?php
+interface Singleton {
+	public static function &getInstance();
+}
+
+interface ObserverSubject {
+	public function registerObserver(Observer &$observer);
+	public function removeObserver(Observer &$observer);
+	public function notifyObservers();
+}
+
+interface Observer {
+	public function register(ObserverSubject &$subject);
+	public function update($update);
+}
+
+interface Converter {
+	public function getXML(DOMDocument $xml);
+	public function convert($data);
+}
+
+abstract class Decorator {
+	protected $decorator = null;
+	
+	abstract public function getXML(DOMDocument $xml);
+	
+	public function getDecorator(){
+		return $this->decorator;
+	}
+	
+	protected function buildXML(DOMDocument $xml, DOMElement $DOMNode){
+		if(!is_null($this->decorator)){
+			$DOMElement = $this->decorator->getXML($xml);
+			$DOMElement->appendChild($DOMNode);
+			return $DOMElement;
+		} else {
+			return $DOMNode;
+		}
+	}
+	
+	public function __sleep(){
+		return array('decorator');
+	}
+}
+?>

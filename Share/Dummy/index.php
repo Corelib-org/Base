88505a9dfdb84a7ce811268bbd772b4072ffcade
Steffen Sørensen <steffen@sublife.dk>
2006-11-06 10:07:25 +0000

    NOTICE:
    index.php have changed to support new pageFactory method
    
    
    
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@3361 516bd117-9ffd-0310-910c-af5d773169db

--- Share/Dummy/index.php
+++ Share/Dummy/index.php
@@ -1,39 +1,15 @@
 <?php
-include_once('etc/config.php');
-
-$url = URLParser::getInstance();
-
-$session = SessionHandler::getInstance();
+require_once('etc/config.php');
+require_once('etc/abstracts.php');
 
 $eventHandler = EventHandler::getInstance();
 $eventHandler->triggerEvent(new EventRequestStart());
 
-include_once('etc/abstracts.php');
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	include_once('etc/post.php');
-	PageFactory::set_no_referer();
-} else {
-	include_once('etc/get.php');
-}
-
-if(!isset($_GET['page'])){
-	$_GET['page'] = '/';
-}
-
-if(substr($_GET['page'], -1) != '/'){
-	$_GET['page'] .= '/';
-}
-
-if(!isset($pages[$_GET['page']])){
-	include_once($pages['/corelib/errors/404/']);
-} else {
-	include_once($pages[$_GET['page']]);
-}
-
+$page = PageFactory::getInstance();
+$page->resolvePageObject();
+		
 try {
 	if(class_exists('WebPage')){
-		$page = PageFactory::getInstance();
 		$page->build(new WebPage());
 	} else {
 		throw new BaseException('Could not find WebPage Class.');

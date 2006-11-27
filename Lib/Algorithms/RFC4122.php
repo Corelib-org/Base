6718bcc7337378da01a770907555993fe32aee3e
Steffen Sørensen <steffen@sublife.dk>
2006-11-27 10:57:04 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@3422 516bd117-9ffd-0310-910c-af5d773169db

---
+++ Lib/Algorithms/RFC4122.php
@@ -0,0 +1,54 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * RFC 4122 A Universally Unique IDentifier (UUID) URN Namespace Generator
+ * 
+ * <i>No Description</i>
+ * 
+ * LICENSE: This source file is subject to version 1.0 of the 
+ * Bravura Distribution license that is available through the 
+ * world-wide-web at the following URI: http://www.bravura.dk/licence/corelib_1_0/.
+ * If you did not receive a copy of the Bravura License and are
+ * unable to obtain it through the web, please send a note to 
+ * license@bravura.dk so we can mail you a copy immediately.
+ * 
+ * @author Steffen Sørensen <steffen@bravura.dk>
+ * @copyright Copyright (c) 2006 Bravura ApS
+ * @license http://www.bravura.dk/licence/corelib_1_0/
+ * @package corelib
+ * @subpackage Base
+ * @link http://www.bravura.dk/
+ * @link http://www.ietf.org/rfc/rfc4122.txt
+ * @version 4.0.0 ($Id$)
+ */
+
+//*****************************************************************//
+//************************ Table Of Content ***********************//
+//*****************************************************************//
+//**                                                        Line **//
+//**    1. RFC4122 Class ...................................     **//
+//**        1. generate Method .............................     **//
+//**                                                             **//
+//*****************************************************************//
+
+//*****************************************************************//
+//************************ RFC 4122 Class *************************//
+//*****************************************************************//
+/**
+ * RFC 4122 A Universally Unique IDentifier (UUID) URN Namespace Generator Class
+ */
+class RFC4122 {
+	/**
+	 * RFC 4122 A Universally Unique IDentifier (UUID) URN Namespace Generator Method
+	 *
+	 * @return string Universally Unique IDentifier (UUID)
+	 */
+	public static function generate(){
+		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		               mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+		               mt_rand( 0, 0x0fff ) | 0x4000,
+		               mt_rand( 0, 0x3fff ) | 0x8000,
+		               mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ));
+	}
+}
+?>

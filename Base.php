d4cf3464fda7740490b669ab43ab2ff778e2ef75
Steffen Sørensen <steffen@sublife.dk>
2006-12-11 16:01:48 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@3452 516bd117-9ffd-0310-910c-af5d773169db

--- Base.php
+++ Base.php
@@ -1,25 +1,26 @@
 <?php
 /* vim: set tabstop=4 shiftwidth=4 softtabstop=4: */
 /**
- *	Corelib Base Functions and Classes
+ * Corelib Base Functions and Classes
  *
- *	<i>No Description</i>
+ * <i>No Description</i>
  *
- *	LICENSE: This source file is subject to version 1.0 of the 
- *	Bravura Distribution license that is available through the 
- *	world-wide-web at the following URI: http://www.bravura.dk/licence/corelib_1_0/.
- *	If you did not receive a copy of the Bravura License and are
- *	unable to obtain it through the web, please send a note to 
- *	license@bravura.dk so we can mail you a copy immediately.
+ * LICENSE: This source file is subject to version 1.0 of the 
+ * Bravura Distribution license that is available through the 
+ * world-wide-web at the following URI: http://www.bravura.dk/licence/corelib_1_0/.
+ * If you did not receive a copy of the Bravura License and are
+ * unable to obtain it through the web, please send a note to 
+ * license@bravura.dk so we can mail you a copy immediately.
  *
  * 
- *	@author Steffen Sorensen <steffen@bravura.dk>
- *	@copyright Copyright (c) 2006 Bravura ApS
- * 	@license http://www.bravura.dk/licence/corelib_1_0/
- *	@package corelib
- *	@subpackage Base
- *	@link http://www.bravura.dk/
- *	@version 4.0.0 ($Id$)
+ * @author Steffen Sorensen <steffen@bravura.dk>
+ * @copyright Copyright (c) 2006 Bravura ApS
+ * @license http://www.bravura.dk/licence/corelib_1_0/
+ * @package corelib
+ * @subpackage Base
+ * @link http://www.bravura.dk/
+ * @version 4.0.0 ($Id$)
+ * @filesource
  */
 
 //*****************************************************************//

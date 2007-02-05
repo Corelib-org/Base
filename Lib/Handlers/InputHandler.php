a70dd6c71feafd62686e7a43566872af05a800e3
Steffen Sørensen <steffen@sublife.dk>
2007-02-05 13:25:46 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@3819 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/Handlers/InputHandler.php
+++ Lib/Handlers/InputHandler.php
@@ -7,7 +7,7 @@ interface InputValidator {
 
 class InputHandler implements Singleton,Output {
 	private static $instance = null;
-	
+
 	private $post_valid = array();
 	private $post = array();
 	private $get_valid = array();
@@ -18,18 +18,18 @@ class InputHandler implements Singleton,Output {
 	private $error_overwrite_post = array();
 	private $serialize_strip_get = array();
 	private $serialize_strip_post = array();
-	
+
 	private function __construct(){
 		if(!defined('INPUT_HANDLER_RESET_GET_POST')){
 			define('INPUT_HANDLER_RESET_GET_POST', true);
 		}
 		set_magic_quotes_runtime(FALSE);
-		if (get_magic_quotes_gpc()) {		
+		if (get_magic_quotes_gpc()) {
 			$this->addslashes = false;
 		} else {
 			$this->addslashes = true;
 		}
-		
+
 		if(isset($_POST)){
 			$this->post = $_POST;
 			if(INPUT_HANDLER_RESET_GET_POST){
@@ -44,11 +44,11 @@ class InputHandler implements Singleton,Output {
 			}
 		}
 	}
-	
+
 	public function addslashes($boolean=true){
-		$this->addslashes = $boolean;	
+		$this->addslashes = $boolean;
 	}
-	
+
 	public function validatePost($item, InputValidator $mode){
 		if(isset($this->post[$item])){
 			if($valid = $this->_validate($this->post[$item], $mode)){
@@ -65,6 +65,7 @@ class InputHandler implements Singleton,Output {
 				return false;
 			}
 		} else {
+			$this->post[$item] = null;
 			$this->input_error = true;
 			return false;
 		}
@@ -80,12 +81,12 @@ class InputHandler implements Singleton,Output {
 		} else {
 			if(isset($this->get_valid[$item])){
 				unset($this->get_valid[$item]);
-			}			
+			}
 			$this->input_error = true;
 			return false;
 		}
 	}
-	
+
 	public function unValidateGet($item){
 		if(isset($this->get_valid[$item])){
 			unset($this->get_valid[$item]);
@@ -93,7 +94,7 @@ class InputHandler implements Singleton,Output {
 		}
 		return false;
 	}
-	
+
 	public function unValidatePost($item){
 		if(isset($this->post_valid[$item])){
 			unset($this->post_valid[$item]);
@@ -101,31 +102,31 @@ class InputHandler implements Singleton,Output {
 		}
 		return false;
 	}
-	
+
 	private function _validate($content, InputValidator $mode){
 		return $mode->validate($content);
 	}
 
 	public function isSetGet($item){
-		return isset($this->get[$item]);	
+		return isset($this->get[$item]);
 	}
 
 	public function isSetPost($item){
-		return isset($this->post[$item]);	
+		return isset($this->post[$item]);
 	}
-	
+
 	public function isValidGet($item){
-		return isset($this->get_valid[$item]);	
+		return isset($this->get_valid[$item]);
 	}
 
 	public function isValidPost($item){
-		return isset($this->post_valid[$item]);	
+		return isset($this->post_valid[$item]);
 	}
-	
+
 	public function getGet($item, $specialchars=true){
 		try {
 			if(!isset($this->get_valid[$item])){
-				throw new BaseException('Variable Not valid');	
+				throw new BaseException('Variable Not valid');
 			} else {
 				if($specialchars){
 					return $this->_htmlspecialchars($this->get_valid[$item], ENT_COMPAT, 'UTF-8');
@@ -139,11 +140,11 @@ class InputHandler implements Singleton,Output {
 		}
 		return false;
 	}
-	
+
 	public function getPost($item, $specialchars=true){
 		try {
 			if(!isset($this->post_valid[$item])){
-				throw new BaseException('Variable Not valid');	
+				throw new BaseException('Variable Not valid');
 			} else {
 				if($specialchars){
 					return $this->_htmlspecialchars($this->post_valid[$item], ENT_COMPAT, 'UTF-8');
@@ -157,7 +158,7 @@ class InputHandler implements Singleton,Output {
 		}
 		return false;
 	}
-	public function setGet($item,$value){	
+	public function setGet($item,$value){
 		$this->get_valid[$item] = $value;
 		$this->get[$item] = $value;
 	}
@@ -165,7 +166,7 @@ class InputHandler implements Singleton,Output {
 		$this->post_valid[$item] = $value;
 		$this->post[$item] = $value;
 	}
-	
+
 	/**
 	 *	@return InputHandler
 	 */
@@ -173,16 +174,16 @@ class InputHandler implements Singleton,Output {
 		if(is_null(self::$instance)){
 			self::$instance = new InputHandler();
 		}
-		return self::$instance;	
+		return self::$instance;
 	}
-	
+
 	public function dumpPost(){
 		return serialize($this->post_valid);
 	}
 	public function dumpGet(){
 		return serialize($this->get_valid);
 	}
-	
+
 	public function importPostDump($dump){
 		$this->post_valid = array_merge($this->post_valid, unserialize($dump));
 		$this->post = array_merge($this->post, unserialize($dump));
@@ -191,10 +192,10 @@ class InputHandler implements Singleton,Output {
 		$this->get_valid = array_merge($this->get_valid, unserialize($dump));
 		$this->get = array_merge($this->get, unserialize($dump));
 	}
-	
+
 	/**
 	 * 	Serialize get array
-	 * 
+	 *
 	 * 	Serialize get Array, and return seriliazed string, with embedded error
 	 * 	codes.
 	 *
@@ -240,10 +241,10 @@ class InputHandler implements Singleton,Output {
 		}
 		return $encoded;
 	}
-	
+
 	/**
 	 * 	Serialize Post Variables
-	 * 
+	 *
 	 *	if $urlencode is set true a url encoded string is returned
 	 * 	otherwise a serialized array is returned
 	 *
@@ -254,51 +255,51 @@ class InputHandler implements Singleton,Output {
 	public function serializePost($urlencode = true){
 		return $this->_serializeArray($this->post, $this->post_valid, $this->error_overwrite_post, $this->serialize_strip_post, $urlencode);
 	}
-	
+
 	/**
 	 * 	Serialize Get Variables
-	 * 
+	 *
 	 *	if $urlencode is set true a url encoded string is returned
 	 * 	otherwise a serialized array is returned
 	 *
 	 * 	@uses InputHandler::_serializeArray()
 	 * 	@param boolean $urlencode if true, variables are url encoded
 	 * 	@return string serialized string
-	 */	
+	 */
 	public function serializeGet($urlencode = true){
 		return $this->_serializeArray($this->get, $this->get_valid, $this->error_overwrite_get, $this->serialize_strip_get, $urlencode);
 	}
-	
+
 	/**
 	 * 	Set Post Error Code
-	 * 
+	 *
 	 *	This is used together with {@link InputHandler::serializePost()},
 	 *  embedding error codes within the serialised string.
 	 *
 	 * 	@see InputHandler::serializePost()
 	 * 	@param boolean $urlencode if true, variables are url encoded
 	 * 	@return boolean true
-	 */	
+	 */
 	public function setPostErrorCode($item, $code){
 		$this->error_overwrite_post[$item] = $code;
 		return true;
 	}
-	
+
 	/**
 	 * 	Set Get Error Code
-	 * 
+	 *
 	 *	This is used together with {@link InputHandler::serializeGet()},
 	 *  embedding error codes within the serialised string.
 	 *
 	 * 	@see InputHandler::serializeGet()
 	 * 	@param boolean $urlencode if true, variables are url encoded
 	 * 	@return boolean true
-	 */	
+	 */
 	public function setGetErrorCode($item, $code){
 		$this->error_overwrite_get[$item] = $code;
 		return true;
 	}
-	
+
 	/**
 	 * 	Strip variable from get array when serializing
 	 *
@@ -319,13 +320,13 @@ class InputHandler implements Singleton,Output {
 		$this->serialize_strip_post[$item] = true;
 		return true;
 	}
-	
+
 	/**
 	 *	Check multiple get variables for validity
-	 * 
+	 *
 	 * 	If no variable names is added, all variables will be checked,
 	 * 	this function can take any number of get variables as arguments
-	 *	
+	 *
 	 *	@return boolean true, if all get variables is valid, else return false
 	 */
 	public function isValidGetVariables($item1=null, $item2=null, $item3=null){
@@ -350,10 +351,10 @@ class InputHandler implements Singleton,Output {
 
 	/**
 	 *	Check multiple post variables for validity
-	 * 
+	 *
 	 * 	If no variable names is added, all variables will be checked,
 	 * 	this function can take any number of post variables as arguments
-	 *	
+	 *
 	 *	@return boolean true, if all post variables is valid, else return false
 	 */
 	public function isValidPostVariables($item1=null, $item2=null, $item3=null){
@@ -375,15 +376,15 @@ class InputHandler implements Singleton,Output {
 		}
 		return true;
 	}
-	
-	
-	public function getXML(DOMDocument $xml){	
+
+
+	public function getXML(DOMDocument $xml){
 		$XMLget = $xml->createElement('get');
 		while(list($key, $val) = each($this->get)){
 			if(preg_match('/^[a-zA-Z0-9_]*$/', $key)){
 				if(is_array($val)) {
 					$XMLArray = $xml->createElement($key);
-					
+
 					while(list($key2, $val2) = each($val)){
 						$XMLItem = $xml->createElement('item', $val2);
 						$XMLItem->setAttribute('id',$key2);
@@ -398,10 +399,10 @@ class InputHandler implements Singleton,Output {
 		reset($this->get);
 		return $XMLget;
 	}
-	
+
 	public function &getArray(){ }
 	public function getString($format = '%1$s'){ }
-	
+
 	private function _addslashes($subject){
 		if(is_array($subject)){
 			while(list($key,$val) = each($subject)){
@@ -413,7 +414,7 @@ class InputHandler implements Singleton,Output {
 		}
 		return $subject;
 	}
-	
+
 	private function _htmlspecialchars($subject, $quote_style=ENT_COMPAT, $charset='UTF-8'){
 		if(is_array($subject)){
 			while(list($key,$val) = each($subject)){
@@ -429,20 +430,20 @@ class InputHandler implements Singleton,Output {
 
 class RegexInputValidator implements InputValidator {
 	private $expr;
-	
+
 	public function __construct($expr){
 		$this->expr = $expr;
 	}
-	
+
 	public function validate($content){
 		return preg_match($this->expr, $content);
-	}	
+	}
 }
 
 class EmailInputValidator implements InputValidator {
 	public function validate($content){
        	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $content));
-	}	
+	}
 }
 class UrlInputValidator implements InputValidator {
 	public function validate($content){
@@ -452,11 +453,11 @@ class UrlInputValidator implements InputValidator {
 
 class EqualsInputValidator implements InputValidator {
 	private $expr;
-	
+
 	public function __construct($expr){
 		$this->expr = $expr;
 	}
-	
+
 	public function validate($content){
 		return ($this->expr == $content);
 	}
@@ -470,7 +471,7 @@ class IsArrayInputValidator implements InputValidator {
 
 class ArrayInputValidator implements InputValidator {
 	private $validator;
-	
+
 	public function __construct($validator){
 		$this->validator = $validator;
 	}

515ab6fc0dea0ce912058455d28e5fb522661528
Steffen Sørensen <steffen@sublife.dk>
2007-02-27 10:57:16 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/branches/3_6_0@3947 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/Database/DatabaseEngines/MySQLi.php
+++ Lib/Database/DatabaseEngines/MySQLi.php
@@ -9,7 +9,7 @@ class MySQLiEngine implements DatabaseEngine {
 	private $reconnect = false;
 
 	const PREFIX = 'MySQLi';
-	
+
 	public function __construct($hostname, $username, $password, $database, $reconnect=false){
 		$this->hostname = $hostname;
 		$this->username = $username;
@@ -21,10 +21,10 @@ class MySQLiEngine implements DatabaseEngine {
 	public function query(Query $query){
 		try {
 			if(!$query instanceof MySQLiQuery){
-				throw new BaseException('Invalid Query Object, object must be instance of MySQLiQuery');	
+				throw new BaseException('Invalid Query Object, object must be instance of MySQLiQuery');
 			}
 		} catch (BaseException $e){
-			echo $e;	
+			echo $e;
 		}
 		if($this->pid != posix_getpid() || is_null($this->connection) || !$this->connection->ping()){
 			$this->_connect();
@@ -62,19 +62,25 @@ class MySQLiEngine implements DatabaseEngine {
 		$this->connection = new mysqli($this->hostname, $this->username, $this->password, $this->database);
 //**
 //**	FOLLOWING LINES ARE REMOVED, BECAUSE OF INCOMBATABILITY WIRH REALLIFELOG
-//**	
+//**
 //**	BREAK NATIVE UTF 8 SUPPORT
 //
-//		$this->connection->query('SET character_set_results = NULL');
-//		$this->connection->query('SET NAMES \'utf8\'');
-//		$this->connection->query('SET CHARACTER_SET utf8');
+
+		if(defined('DATABASE_MYSQLI_ENCODING')){
+			$this->connection->query('SET character_set_results = NULL');
+//			$this->connection->query('SET NAMES \'utf8\'');
+//			$this->connection->query('SET CHARACTER_SET utf8');
+			$this->connection->query('SET NAMES \''.DATABASE_MYSQLI_ENCODING.'\'');
+			$this->connection->query('SET CHARACTER_SET '.DATABASE_MYSQLI_ENCODING);
+		}
+
 		if($this->connection->errno === 0){
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 }
 
 class MySQLiQuery extends Query {
@@ -83,7 +89,7 @@ class MySQLiQuery extends Query {
 	private $error = null;
 	private $errno = null;
 	private $insertid = null;
-	
+
 	public function __construct($query, $limit=null, $limitOffset=null, $order=null, $orderType=null, $count=null){
 		parent::__construct($query, $limit, $limitOffset, $order, $orderType, $count);
 	}
@@ -91,7 +97,7 @@ class MySQLiQuery extends Query {
 		$this->result = $this->instance->query($this->getQuery());
 		$this->error = $this->instance->error;
 		$this->errno = $this->instance->errno;
-		$this->insertid = $this->instance->insert_id;	
+		$this->insertid = $this->instance->insert_id;
 	}
 	public function setInstance($instance){
 		$this->instance = $instance;
@@ -112,7 +118,7 @@ class MySQLiQuery extends Query {
 		return $this->insertid;
 	}
 	public function fetchArray(){
-		return $this->result->fetch_array();	
+		return $this->result->fetch_array();
 	}
 	public function getAffectedRows(){
 		return $this->instance->affected_rows;
@@ -132,42 +138,42 @@ class MySQLiQuery extends Query {
 			$this->query .= ' ORDER by ' . $order;
 			if(strtoupper($orderType) == 'DESC') {
 				$this->query .= ' DESC';
-			}	
+			}
 		}
-	}	
+	}
 	protected function doCount($countKey) {
 		$pos_to = strlen($this->query);
 		$pos_from = stripos($this->query, ' from', 0);
-		
+
 		$pos_group_by = stripos($this->query, ' group by', $pos_from);
-		if (($pos_group_by < $pos_to) && ($pos_group_by != false)) 
+		if (($pos_group_by < $pos_to) && ($pos_group_by != false))
 			$pos_to = $pos_group_by;
-		
+
 		$pos_having = stripos($this->query, ' having', $pos_from);
-		if (($pos_having < $pos_to) && ($pos_having != false)) 
+		if (($pos_having < $pos_to) && ($pos_having != false))
 			$pos_to = $pos_having;
-		
+
 		$pos_order_by = stripos($this->query, ' order by', $pos_from);
-		if (($pos_order_by < $pos_to) && ($pos_order_by != false)) 
-			$pos_to = $pos_order_by;		
-		
+		if (($pos_order_by < $pos_to) && ($pos_order_by != false))
+			$pos_to = $pos_order_by;
+
 		if (stripos($this->query, 'distinct') || stripos($this->query, 'group by')) {
 			$count_string = 'distinct ' . $countKey;
 		} else {
 			$count_string = $countKey;
 		}
 		$count_query = "SELECT COUNT(" . $count_string . ") AS count " . substr($this->query, $pos_from, ($pos_to - $pos_from));
-		
+
 		/*
 		$result = $this->instance->query($count_query);
-		
-		$out = $result->fetch_array();	
+
+		$out = $result->fetch_array();
 		if(isset($out['count']) && !is_null($out['count'])){
 			return $out['count'];
 		} else {
 			return null;
 		}
 		*/
-	}	
+	}
 }
 ?>

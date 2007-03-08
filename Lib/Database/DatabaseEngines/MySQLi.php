7a10b31ce7ca86bc9e35da81e8db442b661e4cb0
Steffen Sørensen <steffen@sublife.dk>
2007-03-08 09:36:20 +0000

    "Fixed charecterset bug in __construct of MySQLiEngine"
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4000 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/Database/DatabaseEngines/MySQLi.php
+++ Lib/Database/DatabaseEngines/MySQLi.php
@@ -10,7 +10,7 @@ class MySQLiEngine implements DatabaseEngine {
 	private $reconnect = false;
 
 	const PREFIX = 'MySQLi';
-	
+
 	public function __construct($hostname, $username, $password, $database, $reconnect=false, $charset='utf8'){
 		$this->hostname = $hostname;
 		$this->username = $username;
@@ -18,14 +18,15 @@ class MySQLiEngine implements DatabaseEngine {
 		$this->database = $database;
 		$this->pid = posix_getpid();
 		$this->reconnect = $reconnect;;
+		$this->charset = $charset;
 	}
 	public function query(Query $query){
 		try {
 			if(!$query instanceof MySQLiQuery){
-				throw new BaseException('Invalid Query Object, object must be instance of MySQLiQuery');	
+				throw new BaseException('Invalid Query Object, object must be instance of MySQLiQuery');
 			}
 		} catch (BaseException $e){
-			echo $e;	
+			echo $e;
 		}
 		if($this->pid != posix_getpid() || is_null($this->connection)){
 			$this->_connect();
@@ -63,13 +64,13 @@ class MySQLiEngine implements DatabaseEngine {
 		$this->connection = new mysqli($this->hostname, $this->username, $this->password, $this->database);
 		if($this->connection->errno === 0){
 			$this->connection->query('SET NAMES '.$this->charset);
-			$this->connection->query('SET CHARACTER SET '.$this->charset);			
+			$this->connection->query('SET CHARACTER SET '.$this->charset);
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 }
 
 class MySQLiQuery extends Query {
@@ -78,7 +79,7 @@ class MySQLiQuery extends Query {
 	private $error = null;
 	private $errno = null;
 	private $insertid = null;
-	
+
 	public function __construct($query, $limit=null, $limitOffset=null, $order=null, $orderType=null, $count=null){
 		parent::__construct($query, $limit, $limitOffset, $order, $orderType, $count);
 	}
@@ -86,7 +87,7 @@ class MySQLiQuery extends Query {
 		$this->result = $this->instance->query($this->getQuery());
 		$this->error = $this->instance->error;
 		$this->errno = $this->instance->errno;
-		$this->insertid = $this->instance->insert_id;	
+		$this->insertid = $this->instance->insert_id;
 	}
 	public function setInstance($instance){
 		$this->instance = $instance;
@@ -107,7 +108,7 @@ class MySQLiQuery extends Query {
 		return $this->insertid;
 	}
 	public function fetchArray(){
-		return $this->result->fetch_array();	
+		return $this->result->fetch_array();
 	}
 	public function getAffectedRows(){
 		return $this->instance->affected_rows;
@@ -127,42 +128,42 @@ class MySQLiQuery extends Query {
 			$this->query .= ' ORDER by ' . $order;
 			if(strtoupper($orderType) == 'DESC') {
 				$this->query .= ' DESC';
-			}	
+			}
 		}
-	}	
+	}
 	protected function doCount($countKey) {
 		$pos_to = strlen($this->query);
 		$pos_from = stripos($this->query, ' from', 0);
-		
+
 		$pos_group_by = stripos($this->query, ' group by', $pos_from);
-		if (($pos_group_by < $pos_to) && ($pos_group_by != false)) 
+		if (($pos_group_by < $pos_to) && ($pos_group_by != false))
 			$pos_to = $pos_group_by;
-		
+
 		$pos_having = stripos($this->query, ' having', $pos_from);
-		if (($pos_having < $pos_to) && ($pos_having != false)) 
+		if (($pos_having < $pos_to) && ($pos_having != false))
 			$pos_to = $pos_having;
-		
+
 		$pos_order_by = stripos($this->query, ' order by', $pos_from);
-		if (($pos_order_by < $pos_to) && ($pos_order_by != false)) 
-			$pos_to = $pos_order_by;		
-		
+		if (($pos_order_by < $pos_to) && ($pos_order_by != false))
+			$pos_to = $pos_order_by;
+
 		if (stripos($this->query, 'distinct') || stripos($this->query, 'group by')) {
 			$count_string = 'distinct ' . $countKey;
 		} else {
 			$count_string = $countKey;
 		}
 		$count_query = "SELECT COUNT(" . $count_string . ") AS count " . substr($this->query, $pos_from, ($pos_to - $pos_from));
-		
+
 		/*
 		$result = $this->instance->query($count_query);
-		
-		$out = $result->fetch_array();	
+
+		$out = $result->fetch_array();
 		if(isset($out['count']) && !is_null($out['count'])){
 			return $out['count'];
 		} else {
 			return null;
 		}
 		*/
-	}	
+	}
 }
 ?>

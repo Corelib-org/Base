73a92d9ad4b1b0f2551dc93db582f65358b9061c
Steffen Sørensen <steffen@sublife.dk>
2008-08-08 11:02:31 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4663 516bd117-9ffd-0310-910c-af5d773169db

--- Share/Dummy/etc/abstracts.php
+++ Share/Dummy/etc/abstracts.php
@@ -1,19 +1,4 @@
 <?php
-/*
- * Setup Language
- */
-if(defined('ABSTRACTS_ENABLE_LANGUAGE') && ABSTRACTS_ENABLE_LANGUAGE){
-	$language = Language::getInstance();
-	$language->setLanguageMap(array('en'));
-	$language->setLanguageFileBase('share/lang/');
-
-	if(!defined('HTTP_STATUS_MESSAGE_FILE')){
-		define('HTTP_STATUS_MESSAGE_FILE', $language->getLanguageFilePath().'status.xml');
-	}
-} else if(!defined('ABSTRACTS_ENABLE_LANGUAGE')){
-	define('ABSTRACTS_ENABLE_LANGUAGE', false);
-}
-
 if(defined('ABSTRACTS_ENABLE_AUTHORIZATION') && ABSTRACTS_ENABLE_AUTHORIZATION){
 	$base->loadClass('UsersAuthorization');
 } else if(!defined('ABSTRACTS_ENABLE_AUTHORIZATION')){

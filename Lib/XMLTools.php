70cebaf89d9b098475798032eef821304cea2d7d
Steffen Sørensen <steffen@sublife.dk>
2008-08-12 09:58:06 +0000

    Added a XML pager to XMLTools
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4681 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/XMLTools.php
+++ Lib/XMLTools.php
@@ -3,5 +3,16 @@ class XMLTools {
 	static public function escapeXMLCharecters($string){
 		return htmlspecialchars($string, null, 'UTF-8', false);
 	}
+	
+	static public function makePagerXML(DOMDocument $xml, $count, $per_page_count=20, $current_page){
+		$pager = $xml->createElement('pager');
+		for ($i = 1; $i <= ceil($count / $per_page_count); $i++){
+			$page = $pager->appendChild($xml->createElement('page', $i));
+			if($i == $current_page){
+				$page->setAttribute('current', 'true');
+			}
+		}
+		return $pager;
+	}
 }
 ?>

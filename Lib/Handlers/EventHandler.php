b16cc821595afeaac7d6e8af25a0d902517bf6e7
Steffen Sørensen <steffen@sublife.dk>
2008-08-14 13:10:50 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4727 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/Handlers/EventHandler.php
+++ Lib/Handlers/EventHandler.php
@@ -5,20 +5,31 @@
  *
  *	<i>No Description</i>
  *
- *	LICENSE: This source file is subject to version 1.0 of the 
- *	Morkland Distribution license that is available through the 
- *	world-wide-web at the following URI: http://www.morkland.com/license/dist_1.0.txt.
- *	If you did not receive a copy of the PHP License and are
- *	unable to obtain it through the web, please send a note to 
- *	license@morkland.com so we can mail you a copy immediately.
+ * This script is part of the corelib project. The corelib project is 
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	@author Steffen S&Oslash;rensen <steffen@morkland.com>
- *	@copyright Copyright &copy; 2005, Morkland
- * 	@license http://www.morkland.com/licence/dist_1.0.txt
- *	@package corelib
- *	@subpackage EventHandler
- *	@link http://www.morkland.com
- *	@version 1.0 Beta ($Id$)
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author Steffen S&Oslash;rensen <steffen@morkland.com>
+ * @copyright Copyright &copy; 2005, Morkland
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @package corelib
+ * @subpackage EventHandler
+ * @link http://www.corelib.org
+ * @version 1.0.1 ($Id$)
  */
 
 class EventHandler implements Singleton,ObserverSubject {
@@ -41,7 +52,6 @@ class EventHandler implements Singleton,ObserverSubject {
 	}
 	
 	public function registerObserver(Observer $observer){
-		// Lets find out about exceptions and but one here, to check for the right type of observer is given
 		try {
 			if(!$observer instanceof EventTypeHandler){
 				throw new BaseException('Invalid InputTypeHandler given');	
@@ -68,10 +78,9 @@ class EventHandler implements Singleton,ObserverSubject {
 	
 	public function triggerEvent(Event $event){
 		if(isset($this->handlers[get_class($event)])){
-			while(list(,$val) = each($this->handlers[get_class($event)])){
+			foreach ($this->handlers[get_class($event)] as $val){
 				$val->update($event);
 			}
-		    reset($this->handlers[get_class($event)]);
 		}
 		return $event;
 	}

ea09dfd213a630f5b6c021c162d72dd075e97f12
Steffen Sørensen <steffen@sublife.dk>
2008-08-14 13:24:52 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4729 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/View.php
+++ Lib/View.php
@@ -1,6 +1,8 @@
 <?php
 define('DATABASE_MYSQLI_VIEW_JOIN_TABLE', 'join_table');
 define('DATABASE_MYSQLI_VIEW_JOIN_KEY', 'join_key');
+define('DATABASE_MYSQLI_VIEW_WHERE', 'where');
+
 define('DATABASE_VIEW_XML_FIELD', 'xml');
 
 class DatabaseViewHelper extends DatabaseListHelper {

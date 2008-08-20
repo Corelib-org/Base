352d6d27779f97392d5850a5bee292b1c6db29e4
Steffen Sørensen <steffen@sublife.dk>
2008-08-20 12:25:10 +0000

    fixed static call
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@4780 516bd117-9ffd-0310-910c-af5d773169db

--- Lib/Algorithms/PasswordGenerator.php
+++ Lib/Algorithms/PasswordGenerator.php
@@ -1,10 +1,11 @@
 <?php
 class PasswordGenerator {
-	private $characters = 'ABCDEFGHKMNPRSTXYZabcdefghjkmnpqrtxyz2346789';
+	private static $characters = 'ABCDEFGHKMNPRSTXYZabcdefghjkmnpqrtxyz2346789';
 	
 	static public function random($len){
 		$code = '';
 		for ($i = 1; $i <= $len; $i++){	
+			$code .= self::$characters{rand(0, (strlen(self::$characters) -1 ))};
 			$code .= $this->characters{mt_rand(0, (strlen($this->characters) -1 ))};
 		}
 		return $code;

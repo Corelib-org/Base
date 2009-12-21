e1a26c383f71f0a1f8dc6a1e198a30cd81af61cd
Steffen Sørensen <steffen@sublife.dk>
2009-12-21 10:47:41 +0000

    Removed old codegenerator
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5111 516bd117-9ffd-0310-910c-af5d773169db

--- lib/Manager/CodeGenerator/Plugins/CodeGeneratorModelList/CodeGeneratorModelList.php
+++
@@ -1,8 +0,0 @@
-<?php
-class CodeGeneratorModelList extends CodeGeneratorPlugin {
-	public function init(){
-		$this->_addFile($this->_createFileInstance('CodeGeneratorModelListFile'));
-		$this->_addFile($this->_createFileInstance('CodeGeneratorModelListFileDAOMySQLi'));
-	}
-}
-?>

79f3b6d1913d0fe5683ba197559bd3f1fafe97db
Steffen Sørensen <steffen@sublife.dk>
2010-05-03 09:08:58 +0000

    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5280 516bd117-9ffd-0310-910c-af5d773169db

--- Share/web/javascript/widgets.js
+++
@@ -1,9 +0,0 @@
-var ManagerWidgetErrorLog = {
-	toggleTrace: function(trace){
-		if (trace.style.display == 'none') {
-			trace.style.display = 'block';
-		} else {
-			trace.style.display = 'none';
-		}
-	}
-}

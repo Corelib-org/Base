430863994a5f8bf9e841f53f81fd3e76df997bdd
Steffen Sørensen <steffen@sublife.dk>
2010-06-01 08:05:44 +0000

    Added close link in developer toolbar
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5310 516bd117-9ffd-0310-910c-af5d773169db

--- lib/PageFactory/PageFactoryDeveloperToolbar.php
+++ lib/PageFactory/PageFactoryDeveloperToolbar.php
@@ -188,7 +188,9 @@ final class PageFactoryDeveloperToolbar implements Singleton {
 				}
 
 				$output  = '<link rel="stylesheet" type="text/css" href="corelib/resource/manager/css/toolbar.css" />';
-				$output .= '<div id="developer-toolbar"><div class="toolbar">'.$toolbar.'</div><div class="data">'.$data.'</div></div>';
+				$output .= '<div id="developer-toolbar">';
+				$output .= '<div style="float: right"><a href="javascript:void(0)" onclick="document.getElementById(\'developer-toolbar\').style.display=\'none\';"><img src="corelib/resource/manager/images/icons/toolbar/close.png" alt="Close toolbar" title="Close toolbar"/></a></div>';
+				$output .= '<div class="toolbar">'.$toolbar.'</div><div class="data">'.$data.'</div></div>';
 				return $output;
 			}
 		}

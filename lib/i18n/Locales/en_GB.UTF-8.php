5970f2c31ebf214b9eb911649df45ba31ffd0b53
Steffen Sørensen <steffen@sublife.dk>
2010-09-24 11:48:48 +0000

    Refactored i18n
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5395 516bd117-9ffd-0310-910c-af5d773169db

---
+++ lib/i18n/Locales/en_GB.UTF-8.php
@@ -0,0 +1,53 @@
+<?php
+/* vim: set tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * Corelib Internationalization Locale en_GB.UTF-8.
+ *
+ * <i>No Description</i>
+ *
+ * This script is part of the corelib project. The corelib project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @category corelib
+ * @package Base
+ * @subpackage i18n
+ *
+ * @author Steffen Sørensen <ss@corelib.org>
+ * @copyright Copyright (c) 2010
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @link http://www.corelib.org/
+ * @version 2.0.0 ($Id$)
+ */
+
+//*****************************************************************//
+//***************** i18nLocale_en_GB_UTF8 class *******************//
+//*****************************************************************//
+/**
+ * i18nLocale_en_GB_UTF8 class.
+ *
+ * @category corelib
+ * @package Base
+ * @subpackage i18n
+ * @since 5.0
+ */
+class i18nLocale_en_GB_UTF8 extends i18nLocale {
+	public function __construct(){
+		parent::__construct('en', 'en_GB.UTF-8');
+	}
+}
+?>

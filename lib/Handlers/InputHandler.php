f12c9e9567283bbb054f224ec591f452e9fbfce0
Steffen Sørensen <steffen@sublife.dk>
2011-12-21 14:47:42 +0000

    Added path input handler
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5540 516bd117-9ffd-0310-910c-af5d773169db

--- lib/Handlers/InputHandler.php
+++ lib/Handlers/InputHandler.php
@@ -1353,4 +1353,80 @@ class InputValidatorModelExists implements InputValidator {
 		return $this->instance;
 	}
 }
+
+//*****************************************************************//
+//******************* InputValidatorXpath class *******************//
+//*****************************************************************//
+/**
+ * InputValidatorXpath validator class.
+ *
+ * Use this class to validate content against a xml element- or document based
+ * on a Xpath query, if the query evals returns 1 or more results it will
+ * eval as true.
+ *
+ * @category corelib
+ * @package Base
+ * @subpackage InputHandler
+ */
+class InputValidatorXpath implements InputValidator {
+
+
+	//*****************************************************************//
+	//************* InputValidatorXpath class properties **************//
+	//*****************************************************************//
+	/**
+	 * Xpath instance.
+	 *
+	 * @var DOMXpath
+	 */
+	private $xpath = null;
+
+	/**
+	 * Xpath query
+	 *
+	 * @var string
+	 */
+	private $query = null;
+
+
+	//*****************************************************************//
+	//************* InputValidatorXpath class methods *****************//
+	//*****************************************************************//
+	/**
+	 * Create new instance.
+	 *
+	 * @param mixed $xml DOMElement or DOMNode to use
+	 * @param string $xpath xpath query to check against, a sprinf format can be used with one argument which refers to the input value
+	 * @return void
+	 * @throws BaseException if first argument is invalid.
+	 * @internal
+	 */
+	public function __construct($xml, $query){
+		$this->query = $query;
+		if($xml instanceof DOMDocument){
+			$this->xpath = new DOMXPath($xml);
+		} else if($xml instanceof DOMElement){
+			$doc = new DOMDocument();
+			$doc->appendChild($doc->importNode($xml, true));
+			$this->xpath = new DOMXPath($doc);
+		} else {
+			throw new BaseException('Invalid argument, first argument must be either DOMDocument or DOMElement', E_USER_ERROR);
+		}
+	}
+
+	/**
+	 * Validate content against xpath query.
+	 *
+	 * @see InputValidator::validate()
+	 * @return boolean true i content is valid, else return false
+	 */
+	public function validate($content){
+		$query = $this->xpath->query(sprintf($this->query, $content));
+		if($query->length > 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
 ?>

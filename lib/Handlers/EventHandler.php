90086b8dea8ea55c3b5d653eaeeda612f3371511
Steffen Sørensen <steffen@sublife.dk>
2012-09-02 09:43:36 +0000

    Added some extra check to show developer toolbar
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5595 516bd117-9ffd-0310-910c-af5d773169db

--- lib/Handlers/EventHandler.php
+++ lib/Handlers/EventHandler.php
@@ -90,7 +90,7 @@ class EventHandler implements Singleton {
 	 * @internal
 	 */
 	private function __construct(){
-		if(BASE_RUNLEVEL >= BASE_RUNLEVEL_DEVEL && PAGE_FACTORY_SHOW_DEVELOPER_TOOLBAR){
+		if(BASE_RUNLEVEL >= BASE_RUNLEVEL_DEVEL && (!defined('PAGE_FACTORY_SHOW_DEVELOPER_TOOLBAR') || PAGE_FACTORY_SHOW_DEVELOPER_TOOLBAR)){
 			$this->log = PageFactoryDeveloperToolbar::getInstance()->addItem(new EventHandlerLogToolbar());
 		}
 	}

2ba08f51f4d4cce291763d9e80785007c49d5dac
Steffen Sørensen <steffen@sublife.dk>
2012-09-11 08:29:42 +0000

    Added Basic file logging engine with basic http info attached
    git-svn-id: http://www.corelib.org/svn/corelib/Base/trunk@5617 516bd117-9ffd-0310-910c-af5d773169db

---
+++ lib/Logger/Engines/HTTPFile.php
@@ -0,0 +1,16 @@
+<?php
+class LoggerEngineHTTPFile extends LoggerEngineFile {
+	private $fp = null;
+
+	protected function _createLogLine($timestamp, $level, $message, $file=null, $line=null, $function=null){
+		$prefix = '';
+
+		if(defined('SESSION_COOKIE_NAME') && isset($_COOKIE[SESSION_COOKIE_NAME])){
+			$prefix .= $_COOKIE[SESSION_COOKIE_NAME].' ';
+		}
+		$prefix .= $_SERVER['REQUEST_METHOD'].' "'.$_SERVER['REQUEST_URI'].'" ';
+
+		return parent::_createLogLine($timestamp, $level, $prefix.$message, $file, $line, $function);
+	}
+}
+?>
